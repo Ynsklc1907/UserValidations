5827bc451c9ed806a89de0adfb98a1a5997ac82a
Ynsklc1907 <Ynsklc1907@gmail.com>
2019-08-22 17:06:51 +0300

    commit3

---
+++ DataAccessLayer/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class UserRepository
+    {
+        public void Add(User entity)
+        {
+            SomethingEntities ctx = new SomethingEntities();
+            entity.IsDeleted = false;
+            ctx.Users.Add(entity);
+            ctx.SaveChanges();
+        }
+    }
+}
---
+++ MySomething/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using DataAccessLayer;
+using MySomething.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Serialization;
+
+namespace MySomething.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser()
+        {
+           
+            UserViewModel model = new UserViewModel();
+           
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult AddUser(UserViewModel usr)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(usr);
+            }
+            //User userDataModel = new User();
+            //userDataModel.CreateDate = usr.CreateDate;
+            //userDataModel.IsDeleted =false;
+            //userDataModel.Name = usr.Name;
+            //userDataModel.Password = usr.Password;
+            //userDataModel.Surname = usr.Surname;
+            //userDataModel.Type = usr.Type;
+            //userDataModel.UserName = usr.Username;
+
+            User userDataModel = new User();
+            string serializedStr = JsonConvert.SerializeObject(usr);
+            userDataModel = JsonConvert.DeserializeObject<User>(serializedStr);
+
+            UserRepository rep = new UserRepository();
+            rep.Add(userDataModel);
+            return RedirectToAction("Index");
+            
+        }
+    }
+}
